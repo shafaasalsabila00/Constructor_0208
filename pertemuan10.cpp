269a435703489006db22252de14e1c7b40eb6899
shafaasalsabila00 <sashaaraabil@gmail.com>
2025-05-17 18:20:11 +0700

    tambah class mahasiswa

--- pertemuan10.cpp
+++ pertemuan10.cpp
@@ -204,3 +204,21 @@ int main() {
 } // Destructor Dipanggil
 
 
+// konstruktor
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+    int nim;
+    string nama;
+
+public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
+};
+
