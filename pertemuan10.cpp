a3a0a9372aceb2ac0b3413174d13531f63ae1303
shafaasalsabila00 <sashaaraabil@gmail.com>
2025-05-17 18:15:56 +0700

    tambah fungsi main

--- pertemuan10.cpp
+++ pertemuan10.cpp
@@ -144,3 +144,12 @@ public:
 
 } bukunya;
 
+int main()
+{
+    // bukunya.setJudul("Matematika");
+    // cout << bukunya.getJudul();
+    cout << bukunya.setJudul("Matematika").getJudul(); // chain function calls
+    return 0;
+}
+
+
