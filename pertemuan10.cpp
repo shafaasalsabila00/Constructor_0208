d7a77ff4b63c70f9341c5b1a0715d3ee6639da76
shafaasalsabila00 <sashaaraabil@gmail.com>
2025-05-17 15:28:34 +0700

    membuat class buku

--- pertemuan10.cpp
+++ pertemuan10.cpp
@@ -4,3 +4,13 @@ using namespace std;
 
 class Petugas;
 class Admin;
+
+class Buku {
+private:
+    string judul;
+    string penulis;
+    bool dipinjam;
+
+public:
+    Buku(string j, string p) : judul(j), penulis(p), dipinjam(false) {}
+};
